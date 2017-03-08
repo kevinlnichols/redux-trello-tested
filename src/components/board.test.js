50b3e11a177c7b2c70c6fa7013a20124aa48f000
Joe Turner <joe@oampo.co.uk>
2017-03-08 13:24:12 +0000

    Fix component tests.

--- src/components/board.test.js
+++ src/components/board.test.js
@@ -1,42 +1,74 @@
 import React from 'react';
 import {shallow} from 'enzyme';
 
-import Board from './board';
+import {Board} from './board';
+import List from './list';
+import {addList} from '../actions';
+
+// Mock the async fetchBoard action
+const mockFetchBoardAction = {
+    type: 'FETCH_BOARD'
+};
+jest.mock('../actions', () => ({
+    ...require.requireActual('../actions'),
+    fetchBoard() {
+        return mockFetchBoardAction;
+    }
+}));
 
 describe('<Board/>', () => {
     let seedLists = [];
     beforeAll(() => {
         for (let i=0; i<10; i++) {
-            seedLists.push(`List ${i}`)
+            seedLists.push({
+                title: `List ${i}`,
+                cards: []
+            })
         }
     });
 
     it('Renders without crashing', () => {
-        shallow(<Board title="Foo" />);
+        const dispatch = jest.fn();
+        shallow(<Board title="Foo" lists={[]} dispatch={dispatch} />);
+    });
+
+    it('Dispatches fetchBoard on mount', () => {
+        const dispatch = jest.fn();
+        shallow(<Board title="Foo" lists={[]} dispatch={dispatch} />);
+        expect(dispatch).toHaveBeenCalled();
+        expect(dispatch.mock.calls[0][0]).toEqual(mockFetchBoardAction);
     });
 
     it('Renders the title', () => {
+        const dispatch = jest.fn();
         const title = "Foo";
-        const wrapper = shallow(<Board title={title} />);
+        const wrapper = shallow(
+            <Board title={title} lists={[]} dispatch={dispatch} />
+        );
         expect(wrapper.contains(<h2>{title}</h2>)).toEqual(true);
     });
 
-
-    it('Can add lists to the state', () => {
-        const wrapper = shallow(<Board/>);
+    it('Dispatches addList from addList', () => {
+        const dispatch = jest.fn();
+        const wrapper = shallow(
+            <Board lists={[]} dispatch={dispatch} />
+        );
+        // Ignore any previous calls to dispatch
+        dispatch.mockClear();
         const instance = wrapper.instance();
-        seedLists.forEach(instance.addList);
-        expect(wrapper.state('lists').length).toEqual(seedLists.length);
+        const title = seedLists[0].title;
+        instance.addList(title);
+        expect(dispatch).toHaveBeenCalled();
+        expect(dispatch.mock.calls[0][0]).toEqual(addList(title));
     });
 
     it('Renders the lists', () => {
-        const wrapper = shallow(<Board/>);
-        const instance = wrapper.instance();
-        seedLists.forEach(instance.addList);
-        const lists = wrapper.find('List');
+        const dispatch = jest.fn();
+        const wrapper = shallow(<Board lists={seedLists} dispatch={dispatch} />);
+        const lists = wrapper.find(List);
         expect(lists.length).toEqual(seedLists.length);
         const firstList = lists.first();
-        expect(firstList.prop('title')).toEqual(seedLists[0]);
+        expect(firstList.prop('title')).toEqual(seedLists[0].title);
     });
 });
 
